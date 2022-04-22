e77335a2e3662f749fec32a229562603595e1c77
Your Name <bilol200414@gmail.com>
2022-04-22 19:48:36 +0500

    profile pages

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "../steyls/profile.css"
+import NavbarTwo from '../components/NavbarTwo'
+import ozm from "../images/ozm.jpg"
+
+
+function Profile() {
+  return (
+<>
+    <NavbarTwo />
+    <section className="profile">
+      <div className="container">
+        <div className="profile_nav">
+          <div className="profile_left">
+            <img src={ozm} alt="" />
+            <div>
+              <h2>Biloliddin Abduvohiddov</h2>
+              <p>@Biloliddin1150.com</p>
+            </div>
+          </div>
+          <div className="profile_right">
+            <Link to="/">
+              <i class="fa-solid fa-gear"></i> Settings
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+</>
+
+  )
+}
+
+export default Profile
