701fccd3b87bc1a6dda885fc6f53bb9f7f61b81d
Your Name <bilol200414@gmail.com>
2022-04-22 18:57:36 +0500

    logo imges

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { InfoProvider } from "./context/Context.jsx";
+import Home from "./pages/Home.jsx"
+import Register from "./pages/Register.jsx";
+import Profile from "./pages/Profile.jsx"
+import Order from './pages/Order.jsx';
+
+import './steyls/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InfoProvider>
+      <BrowserRouter>
+        <div className='App'>
+          <Switch>
+           
+            <Route exact path="/" component={Home} />
+            <Route path="/Register" component={Register} />
+            <Route path="/Order" component={Order} />
+            <Route path="/Profile" component={Profile} />
+            {/* <Profile /> */}
+          </Switch>
+  
+        </div>
+      </BrowserRouter>
+    </InfoProvider>
   );
 }
 
