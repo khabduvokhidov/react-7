a7c57ebb38431589dbac4145272519cabdf220c7
Your Name <bilol200414@gmail.com>
2022-04-22 19:04:44 +0500

    languages

---
+++ src/languages/Languages.jsx
@@ -0,0 +1,16 @@
+const uz = {
+    header: "Bu saytning header qismi",
+    main: "Bu saytning Main qismi",
+    footer: "Bu saytninh Footer qismi"
+  }
+  
+  const eng = {
+    header: "This is Header",
+    main: "This is Main",
+    footer: "This is Footer"
+  }
+  
+  export default {
+    uz,
+    eng,
+  }
