5a75068c7bb46cfd6951550ffe8648976d086e59
Your Name <bilol200414@gmail.com>
2022-04-25 14:13:08 +0500

    App js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Home from "./pages/Home.jsx"
 import Register from "./pages/Register.jsx";
 import Profile from "./pages/Profile.jsx"
 import Order from './pages/Order.jsx';
+import Partfolio from "./pages/Partfolio.jsx";
+import PaidCursor from "./pages/PaidCursor.jsx"
+import Settings from "./pages/Setings.jsx"
 
 import './steyls/App.css';
+import Chat from './pages/Chat.jsx';
 
 function App() {
   return (
@@ -15,11 +19,15 @@ function App() {
         <div className='App'>
           <Switch>
            
-            <Route exact path="/" component={Home} />
-            <Route path="/Register" component={Register} />
+            {/* <Route exact path="/" component={Home} />
+            <Route path="/Partfolio" component={Partfolio} />
             <Route path="/Order" component={Order} />
             <Route path="/Profile" component={Profile} />
-            {/* <Profile /> */}
+            <Route path="/PaidCursor" component={PaidCursor} />
+            <Route path="/Register" component={Register} /> 
+            <Route path="/Settings" component={Settings} /> */}
+            <Chat />
+ 
           </Switch>
   
         </div>
